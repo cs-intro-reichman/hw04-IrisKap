024dfed684dafe7566d46ee9c0703f2b314506eb
Iris Kaplun <iris.kaplun@post.runi.ac.il>
2024-01-18 19:36:12 +0200

    first submittion

--- ArrayOps.java
+++ ArrayOps.java
@@ -1,26 +1,114 @@
 public class ArrayOps {
     public static void main(String[] args) {
+
+      int [] array1 =     {3, -4, 1, 5};
+      int [] array2 =    {1, 3, -4, 5, 2, 9, 5, 3};
+      boolean result = containsTheSameElements(array1 , array2);
+      System.out.println(result);
         
     }
     
     public static int findMissingInt (int [] array) {
-        // Write your code here:
-        return -1;
+        int length = array.length;
+        int num;
+        boolean[] isInArrey = new boolean[length+1];
+        for(int i = 0; i < length; i++)
+        {
+            num = array[i];
+            isInArrey[num] = true;
+        } 
+        int i = 0;
+        while(isInArrey[i])
+        {
+            i++;
+        }
+        return i;
     }
 
     public static int secondMaxValue(int [] array) {
-        // Write your code here:
-        return 0;
+        int max1 = Math.max(array[0],array[1]);
+        int max2 = Math.min(array[0],array[1]);
+
+        for(int i=2 ; i< array.length; i++ )
+        {   
+            if(array[i] > max2)
+            {
+                if(array[i]> max1)
+                {
+                    max2 = max1;
+                    max1 = array[i];
+                }
+                else max2 = array[i];
+            }
+
+        }
+        return max2;
     }
 
     public static boolean containsTheSameElements(int [] array1,int [] array2) {
-        // Write your code here:
-        return false;
+        int i =0;
+        boolean foundFirst = true;
+        while(i < array1.length && foundFirst == true)
+        {   
+            foundFirst = false;
+            int j =0;
+            while(foundFirst == false && j < array2.length)
+            {
+                if(array2[j] == array1[i])
+                {
+                    foundFirst = true;
+                }
+                j++;
+
+            }
+            i++;
+        }
+        i =0;
+        boolean foundSecond = true;
+        while(i < array2.length && foundSecond == true)
+        {   
+            foundSecond = false;
+            int j =0;
+            while(foundSecond == false && j < array1.length)
+            {
+                if(array1[j] == array2[i])
+                {
+                    foundSecond = true;
+                }
+                j++;
+
+            }
+            i++;
+        }
+        return foundFirst && foundSecond;
     }
 
     public static boolean isSorted(int [] array) {
-        // Write your code here:
-        return false;
+        boolean sorted = true;
+        boolean increasing = false;
+        boolean decreasing = false;
+        int i=0;
+        while(!increasing && !decreasing && i< array.length-1)
+        {
+            if(array[i]> array[i+1])
+                decreasing = true;
+            else if(array[i]< array[i+1])
+                increasing = true;
+            i++;
+        }
+        while(increasing && sorted && i< (array.length-1))
+        {
+            if(array[i] > array [i+1])
+                sorted = false;
+            i++;
+        }
+        while(decreasing && sorted && i< (array.length-1))
+        {
+            if(array[i] < array [i+1])
+                sorted = false;
+            i++;
+        }
+        return sorted;
     }
 
 }
--- StringOps.java
+++ StringOps.java
@@ -22,17 +22,126 @@ public class StringOps {
     //////                                               ///////
     ////////////////////////////////////////////////////////////
     public static void main(String[] args) {
+        String test = "   First tWo wordS";
+        System.out.println(camelCase(test));
+    //    System.out.println( lowerCase('W'));
+    //    System.out.println( lowerCase('a'));
+    //    System.out.println( upperCase('W'));
+    //    System.out.println( upperCase('a'));
+    }
+    
+    
+
+     public static char lowerCase (char letter) {
+       
+        if(letter <= 'Z' && letter >= 'A')
+            return (char)(letter+32);
+        else return letter;
+    }
+
+    public static char upperCase (char letter) {
         
+        if(letter <= 'z' && letter >= 'a')
+            return (char)(letter-32);
+        else return letter;
+    }
+
+    public static boolean IsVowel (char letter) {
+        
+        if(letter == 'o' || letter == 'i' ||letter == 'a' ||letter == 'e' ||letter == 'u')
+            return true;
+        if(letter == 'O' || letter == 'I' ||letter == 'A' ||letter == 'E' ||letter == 'U')
+            return true;
+        else return false;
     }
 
     public static String capVowelsLowRest (String string) {
-        // Write your code here:
-        return "";
+        int length = string.length();
+        String temp = "";
+
+        if(IsVowel(string.charAt(0)))
+        {
+            temp += upperCase(string.charAt(0));
+            temp += string.substring(1);
+        }
+        else 
+        {
+            temp += lowerCase(string.charAt(0));
+            temp += string.substring(1);
+        }
+        for(int i =1; i<length-1 ; i++)
+        {   string = temp;
+            temp = "";
+            if(IsVowel(string.charAt(i)))
+            {
+                temp += string.substring(0, i);
+                temp += upperCase(string.charAt(i));
+                temp += string.substring(i+1);
+            }
+            else 
+            {
+                temp += string.substring(0, i);
+                temp += lowerCase(string.charAt(i));
+                temp += string.substring(i+1);
+            }
+        }
+        string = temp;
+        temp = "";
+        if(IsVowel(string.charAt(length -1)))
+        {
+            temp += string.substring(0, length-1);
+            temp += upperCase(string.charAt(length -1));
+            
+        }
+        else 
+        {
+            temp += string.substring(0, length-1);
+            temp += lowerCase(string.charAt(length -1));
+        }
+        return temp;
+
+    }
+    public static int nextWhiteSpace (String string)
+    {
+        int i=0;
+        while(string.charAt(i)!= ' ' && i != string.length()-1)
+        {
+            i++;
+        }
+        return i;
     }
 
     public static String camelCase (String string) {
-        // Write your code here:
-        return "";
+        int j=0;
+        while(string.charAt(j)== ' ')
+            j++;
+        
+        int firstWhiteSpace = j + nextWhiteSpace(string.substring(j));//5
+        int i=j;
+        String newString = "";
+        while(i< firstWhiteSpace)
+        {
+            newString+= lowerCase(string.charAt(i));
+            i++;
+        }
+
+        int nextWhiteSpace = firstWhiteSpace;
+
+        while(nextWhiteSpace!= string.length()-1)
+        {
+            newString+= upperCase(string.charAt(nextWhiteSpace+1));
+            int tempIndex = nextWhiteSpace +2;
+            nextWhiteSpace += nextWhiteSpace(string.substring(nextWhiteSpace+2)) +2;
+            i = tempIndex;
+            while(i<nextWhiteSpace)
+            {
+                newString+= lowerCase(string.charAt(i));
+                i++;
+            }
+            //newString+= string.substring(tempIndex, nextWhiteSpace);
+        }
+        newString+= lowerCase(string.charAt(string.length()-1));
+        return newString;
     }
 
     public static int[] allIndexOf (String string, char chr) {
